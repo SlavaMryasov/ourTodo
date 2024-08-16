b9f5f0ad5d9d391471097a97f1d195d6c1d586f5
SlavaMryasov <slavadevbox1@gmail.com>
2024-08-16 11:54:00 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='min-h-screen bg-slate-400'>
+      <div className='w-1/3 p-2 bg-slate-300 rounded' >
+        <form action="">
+          <input type="text" className='forAddNewTodoName' />
+          <button className='forAddNewTodo'>add</button>
+        </form>
+        <ul className='for todos'>
+          <li><span>task1</span> <button>delete</button></li>
+          <li><span>task1</span> <button>delete</button></li>
+          <li><span>task1</span> <button>delete</button></li>
+
+        </ul>
+        <div>
+          <button>all</button>
+          <button>completed</button>
+          <button>uncompleted</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
 export default App
+
+
